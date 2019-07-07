65b1d69033ed55497ae2cc98b2255a70d013ff7a
Isaac Salcedo <isalcedo16@aol.com>
2019-07-06 22:06:58 -0400

    Styles in React.

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import './media.css';
 
 class Media extends Component {
   render() {
+    const styles = {
+      container: {
+        color: '#44546b',
+        cursor: 'pointer',
+        width: 260,
+        border: '1px solid red'
+      }
+    }
     return (
-      <div>
-        <div>
-          <img src="" alt="" width={260} height={160} />
-          <h3>¿Por qué aprender React?...</h3>
-          <p>Isaac Salcedo</p>
+      <div className="Media">
+        <div className="Media-cover">
+          <img src="./images/covers/bitcoin.jpg"
+            alt="" width={260} height={160} />
+          <h3 className="Media-title">¿Por qué aprender React?...</h3>
+          <p className="Media-author">Isaac Salcedo</p>
         </div>
       </div>
     );
