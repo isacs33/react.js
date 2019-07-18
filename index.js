7d15dae05a5ca1766cf76a66a08fd6460bbd5f99
Isaac Salcedo <isalcedo16@aol.com>
2019-07-18 00:36:10 -0400

    Lists in React.

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Media from './src/playlist/components/media';
+import Playlist from './src/playlist/components/playlist';
+import data from './src/api.json';
 
 // console.log("Hola, mundo!")
 
 const $app = document.getElementById('app');
 // const $holaMundo = <h1>Hola, mundo!!!</h1>;
 
-render(
-  <Media
-    type="video"
-    title="¿Qué es responsive design?"
-    author="Isaac Salcedo"
-    image="./images/covers/responsive.jpg"
-  />,
-  $app
-);
+render(<Playlist data={data} />, $app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -23,9 +23,9 @@ class Media extends Component {
       <div className="Media" onClick={this.handleClick}>
         <div className="Media-cover">
           <img
-            src={this.props.image}
+            src={this.props.cover}
             alt=""
-            width={260}
+            width={240}
             height={160}
           />
           <h3 className="Media-title">{this.props.title}</h3>
@@ -37,7 +37,7 @@ class Media extends Component {
 }
 
 Media.propTypes = {
-  image: PropTypes.string,
+  cover: PropTypes.string,
   title: PropTypes.string.isRequired,
   author: PropTypes.string,
   type: PropTypes.oneOf(['video', 'audio'])
