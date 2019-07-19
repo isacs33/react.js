f583437d5e40f136542ffe02e417331893e0b1ee
Isaac Salcedo <isalcedo16@aol.com>
2019-07-19 09:22:00 -0400

    Component Composition.

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Media from './media';
 import './playlist.css';
+import Play from '../../icons/components/play.js';
 
 class Playlist extends Component {
   render() {
@@ -8,6 +9,7 @@ class Playlist extends Component {
     console.log(this.props.data);
     return (
       <div className="Playlist">
+        <Play size={100} color="red" />
         {
           playlist.map((item) => {
             return <Media {...item} key={item.id}/>
