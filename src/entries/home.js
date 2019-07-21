4b5d34ea326218bc5620eb2ab5437fb647bb8b46
Isaac Salcedo <isalcedo16@aol.com>
2019-07-21 18:41:07 -0400

    Proyect Structure. Containers and UI. Categories.

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
-// import data from './src/api.json';
+import data from '../api.json';
 
 // console.log("Hola, mundo!")
 
 const $app = document.getElementById('app');
 // const $holaMundo = <h1>Hola, mundo!!!</h1>;
 
-render(<Home />, $app);
+render(<Home data={data} />, $app);
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,29 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './media';
 import './playlist.css';
-import Play from '../../icons/components/play.js';
-import Pause from '../../icons/components/pause.js';
-import Volume from '../../icons/components/volume.js';
-import FullScreen from '../../icons/components/fullscreen.js';
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist;
-    console.log(this.props.data);
-    return (
-      <div className="Playlist">
-        <Play size={100} color="red" viewBox="0 0 32 32"/>
-        <Pause size={100} color="blue" viewBox="0 0 32 32"/>
-        <Volume size={100} color="green" viewBox="0 0 34 32"/>
-        <FullScreen size={100} color="orange" viewBox="0 0 32 32"/>
-        {
-          playlist.map((item) => {
-            return <Media {...item} key={item.id}/>
-          })
-        }
-      </div>
-    )
-  }
+function Playlist(props) {
+  return (
+    <div className="Playlist">
+      {
+        props.playlist.map((item) => {
+          return <Media {...item} key={item.id}/>
+        })
+      }
+    </div>
+  )
 }
 
 export default Playlist;
