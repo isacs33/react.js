de456a7ef8554946521dc06a56f696b770d1ac21
Isaac Salcedo <isalcedo16@aol.com>
2019-07-21 17:57:46 -0400

    Proyect Structure. Containers and UI Components. Web site's Layout.

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Playlist from './src/playlist/components/playlist';
-import data from './src/api.json';
+import Home from '../pages/containers/home';
+// import Playlist from './src/playlist/components/playlist';
+// import data from './src/api.json';
 
 // console.log("Hola, mundo!")
 
 const $app = document.getElementById('app');
 // const $holaMundo = <h1>Hola, mundo!!!</h1>;
 
-render(<Playlist data={data} />, $app);
+render(<Home />, $app);
