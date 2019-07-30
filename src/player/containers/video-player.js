c3eeba3b10b4ecd9ecf5364742558a37034f2a06
Isaac Salcedo <isalcedo16@aol.com>
2019-07-30 17:37:42 -0400

    Spinner.

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -6,13 +6,15 @@ import PlayPause from '../components/play-pause';
 import Timer from '../components/timer';
 import Controls from '../components/video-player-controls';
 import ProgressBar from '../components/progress-bar';
+import Spinner from '../components/spinner';
 import formattedTime from '../../lib/format-time';
 
 class VideoPlayer extends Component {
   state = {
     pause: true,
     duration: 0,
-    currentTime: 0
+    currentTime: 0,
+    loading: false
   }
   togglePlay = (event) => {
     this.setState({
@@ -39,6 +41,16 @@ class VideoPlayer extends Component {
   handleProgressChange = event => {
     this.video.currentTime = event.target.value;
   }
+  handleSeeking = event => {
+    this.setState({
+      loading: true
+    })
+  }
+  handleSeeked = event => {
+    this.setState({
+      loading: false
+    })
+  }
   render() {
     return (
       <VideoPlayerLayOut>
@@ -60,11 +72,14 @@ class VideoPlayer extends Component {
             handleProgressChange={this.handleProgressChange}
           />
         </Controls>
+        <Spinner active={this.state.loading} />
         <Video
           autoplay={this.props.autoplay}
           pause={this.state.pause}
           handleLoadedMetadata={this.handleLoadedMetadata}
           handleTimeUpdate={this.handleTimeUpdate}
+          handleSeeking={this.handleSeeking}
+          handleSeeked={this.handleSeeked}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayOut>
