69e3eb9a21cb4624d053d469919e85a29994726f
Isaac Salcedo <isalcedo16@aol.com>
2019-07-30 01:12:54 -0400

    Title and Video Font Components.

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import VideoPlayerLayOut from '../components/video-player-layout';
+import Video from '../components/video';
+import Title from '../components/title';
 
 class VideoPlayer extends Component {
   render() {
     return (
       <VideoPlayerLayOut>
-        <video
-          controls
-          autoPlay
+        <Title
+          title="Esto es un video chido!"
+        />
+        <Video
+          autoplay={false}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayOut>
